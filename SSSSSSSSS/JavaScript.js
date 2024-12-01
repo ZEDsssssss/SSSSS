18dd3c274c44a72915cefee04cc927fc8c8e71be
ZEDsssssss <ken13142007@gmail.com>
2024-12-01 20:55:08 +0800

    Add files via upload

---
+++ SSSSSSSSS/JavaScript.js
@@ -0,0 +1,5 @@
+﻿window.onload = function () {
+    alert("Surprise!!");
+    const videoUrl = "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1";
+    document.getElementById("youtubeVideo").src = videoUrl;
+};
